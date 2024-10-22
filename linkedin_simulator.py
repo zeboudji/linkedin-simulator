e04d32286513dcd31347871b461d051618d6be85
zeboudji <youssouf.zeboudji@insidegroup.fr>
2024-10-22 12:13:41 +0200

    Update linkedin_simulator.py

--- linkedin_simulator.py
+++ linkedin_simulator.py
@@ -265,7 +265,7 @@ global_score = (
 ) * 100  # Pour obtenir un score sur 100
 
 # --- Définition des seuils pour la performance globale ---
-global_performance_thresholds = [35, 60, 80]
+global_performance_thresholds = [10, 25, 45]
 global_performance_labels = ["😟", "😐", "🙂", "🔥"]  # Médiocre, Correct, Bon, Excellent
 
 # --- Détermination de la performance globale ---
